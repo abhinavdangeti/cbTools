d17333b3ae11a5d1e6b794a7a3254cfe6b706b64
abhinavdangeti <abhinav@couchbase.com>
2014-05-13 16:44:49 -0700

    GetsWithLock

---
+++ GetsWithLock/Helper.java
@@ -0,0 +1,57 @@
+import java.net.URI;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.couchbase.client.CouchbaseClient;
+import com.couchbase.client.CouchbaseConnectionFactoryBuilder;
+
+public class Helper {
+
+    private static String _addr = "127.0.0.1";
+    private static String _port = "9000";
+    private static String _bkt = "loadtest";
+    private static String _pass = "";
+    private static int _count = 5;
+    private static String _prefix = "key+";
+
+    public static void main(String args[]) throws InterruptedException {
+        CouchbaseClient client = connect();
+        int i;
+        for(i = 0; i < _count; i++) {
+            client.set(_prefix + i, "ABCD");
+        }
+
+        Thread.sleep(5000);
+
+        i = 0;
+        while (true) {
+            i = i % _count;
+            Object cake = client.getAndLock(_prefix + i, 1);
+            if (cake == null) {
+                System.out.println("Failed to retrieve: " + (_prefix + i));
+            } else {
+                System.out.println(cake.toString());
+            }
+            i++;
+            Thread.sleep(2000);
+        }
+
+    }
+
+    private static CouchbaseClient connect() {
+        /*
+         * CouchbaseClient Connection to bucket
+         */
+        List<URI> uris = new LinkedList<URI>();
+        uris.add(URI.create(String.format("http://" + _addr + ":" + _port + "/pools")));
+        CouchbaseConnectionFactoryBuilder cfb = new CouchbaseConnectionFactoryBuilder();
+        try {
+            return new CouchbaseClient(cfb.buildCouchbaseConnection(uris, _bkt, _pass));
+        } catch (Exception e) {
+            System.err.println("Error connecting to Couchbase: "
+                    + e.getMessage());
+            System.exit(0);
+        }
+        return null;
+    }
+}
---
+++ GetsWithLock/README.md
@@ -0,0 +1,12 @@
+README
+======
+
+(Tool that loads "count" no. of items, and fetches the item list one at a
+time with a lock for 1 second infinitely, waits for 2 seconds between
+every operation)
+
+- Make cluster configuration pertaining changes in Helper.java
+- Run the following commands:
+- To compile: make compile
+- To run: make run
+- To clean: make clean
